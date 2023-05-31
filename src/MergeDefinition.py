1c8eebbf69697734765ede75c202c4e2c3635b52
128na <simutrans.128na@gmail.com>
2023-05-31 23:56:26 +0900

    とりあえず動く

--- src/MergeDefinition.py
+++ src/MergeDefinition.py
@@ -1,19 +1,164 @@
 import json
 import sys
+from PIL import Image, ImageDraw, ImageColor
+from SpecialColor import (
+    SPECIAL_COLORS,
+    TRANSPARENT_COLOR,
+)
+from typing import Final
 
-# todo jsonデータの型をいい感じに指定したい
-class MergeDefinition:
-    def __init__(self, path:str) -> None:
+
+class BaseProcessor:
+    def __init__(self, data: dict) -> None:
+        self.modifyPixel: Final[bool] = False
+        self.name: Final[str] = data["name"]
+        self.comment: Final[str] | None = data.get("comment")
+
+
+class ImageProcessor(BaseProcessor):
+    "画像単位の操作"
+
+    def handleImage(self, canvas: Image.Image) -> Image.Image:
+        return canvas
+
+
+class PixelProcessor(BaseProcessor):
+    "pixel単位の操作"
+
+    def handlePixel(
+        self,
+        draw: ImageDraw.ImageDraw,
+        xy: tuple[int, int],
+        pixel: tuple[int, int, int, int],
+    ) -> None:
+        pass
+
+
+class MergeImage(ImageProcessor):
+    "画像合成"
+
+    def __init__(self, data: dict) -> None:
+        super().__init__(data)
+        self.pathes: Final[list[str]] = data["pathes"]
+        self.mode: Final[str] = data["mode"]
+        self.offset: Final[tuple[int, int]] = (data["offset"]["x"], data["offset"]["y"])
+
+    def handleImage(self, canvas):
+        for path in self.pathes:
+            addImage = Image.open(path)
+            if addImage.size > canvas.size:  # 追加画像が大きい場合はキャンバスを拡大する
+                canvas = self.doResize(canvas, addImage.size)
+            if addImage.size < canvas.size:  # 追加画像が小さい場合はキャンバスのサイズに拡大する
+                addImage = self.doResize(addImage, canvas.size)
+            if self.offset != (0, 0):  # オフセット指定があれば追加画像をずらす
+                addImage = self.doOffset(addImage)
+            sys.stdout.write("\t\t'{0}' merged.\n".format(path))
+            canvas = Image.alpha_composite(
+                canvas.convert("RGBA"), addImage.convert("RGBA")
+            )
+        return canvas
+
+    def doResize(self, original: Image.Image, size: tuple[int, int]) -> Image.Image:
+        newImage = Image.new("RGBA", size)
+        newImage.paste(original, (0, 0), original)
+        return newImage
+
+    def doOffset(self, original: Image.Image) -> Image.Image:
+        newImage = Image.new("RGBA", original.size)
+        newImage.paste(original, self.offset, original)
+        return newImage
+
+
+class RemoveTransparent(PixelProcessor):
+    "透明->透過色"
+
+    def __init__(self, data: dict) -> None:
+        super().__init__(data)
+        self.threthold: Final[int] = data["threthold"]
+
+    def handlePixel(self, draw, xy, pixel):
+        if pixel[3] == 255:  # 不透過はそのまま
+            return draw
+        if pixel[3] >= self.threthold:  # 半透過は閾値以上は塗りつぶす
+            return draw.point(xy, (pixel[0], pixel[1], pixel[2], 255))
+        else:  # 閾値未満は透過色にする
+            return draw.point(xy, TRANSPARENT_COLOR)
+
+
+class ReplaceColor(PixelProcessor):
+    "指定色置換"
+
+    def __init__(self, data: dict) -> None:
+        super().__init__(data)
+        self.search: tuple[int, int, int] = (
+            data["search"]["r"],
+            data["search"]["g"],
+            data["search"]["b"],
+        )
+        self.replace: tuple[int, int, int] = (
+            data["replace"]["r"],
+            data["replace"]["g"],
+            data["replace"]["b"],
+        )
+
+    def handlePixel(self, draw, xy, pixel):
+        if pixel == self.search:
+            draw.point(xy, self.replace)
+
+
+class RemoveSpecialColor(PixelProcessor):
+    "特殊色削除"
+
+    def __init__(self, data: dict) -> None:
+        super().__init__(data)
+
+    def handlePixel(self, draw, xy, pixel):
+        if (pixel[0],pixel[1],pixel[2]) in SPECIAL_COLORS:
+            draw.point(
+                xy,
+                (
+                    min(255, pixel[0] + 1),
+                    min(255, pixel[1] + 1),
+                    min(255, pixel[2] + 1),
+                ),
+            )
+
+
+class Definition:
+    def __init__(self, data: dict) -> None:
+        self.outputPath: str = data["outputPath"]
+        self.processors: list[ImageProcessor | PixelProcessor] = list(
+            map(self.processorFactory, data["rules"])
+        )
+        self.comment: str | None = data.get("comment")
+
+    def processorFactory(self, rule: dict) -> ImageProcessor | PixelProcessor:
+        match rule["name"]:
+            case "mergeImage":
+                return MergeImage(rule)
+            case "removeTransparent":
+                return RemoveTransparent(rule)
+            case "replaceColor":
+                return ReplaceColor(rule)
+            case "removeSpecialColor":
+                return RemoveSpecialColor(rule)
+
+        raise KeyError("{0} is not defined rule.".format(rule["name"]))
+
+
+class MergeDefinitionLoader:
+    def __init__(self, path: str) -> None:
         self.path = path
-        self.load()
+        file = open(self.path, "r")
+        data: dict = json.load(file)
+        self.version: int = data["version"]
+        self.comment: str | None = data.get("comment")
+        self.definitions: list[Definition] = list(map(Definition, data["definitions"]))
+        sys.stdout.write(
+            "'{0}' load successed.\ndefinition version is {1}.\n".format(
+                self.path, self.version
+            )
+        )
 
-    def load(self):
-        file = open(self.path, 'r')
-        self.json = json.load(file)
-        sys.stdout.write("'{0}' load successed.\ndefinition version is {1}.\n".format(self.path, self.getVersion()))
-
-    def getVersion(self) -> int:
-        return self.json['version']
-
-    def getDefinitions(self) -> list[list]:
-        return self.json['definitions']
+    def getDefinitions(self) -> list[Definition]:
+        return self.definitions
--- src/simutrans-image-merger.py
+++ src/simutrans-image-merger.py
@@ -1,11 +1,70 @@
 import sys
+from typing import Final
+
+from PIL import Image, ImageDraw
+from MergeDefinition import (
+    MergeDefinitionLoader,
+    Definition,
+    ImageProcessor,
+    PixelProcessor,
+)
+
+import psutil
+
 
 def run():
-    if(len(sys.argv) < 2):
+    if len(sys.argv) < 2:
         raise Exception("json file path not provided.")
 
-    filename = sys.argv[1]
+    definitions: Final[list[Definition]] = MergeDefinitionLoader(
+        sys.argv[1]
+    ).getDefinitions()
+
+    for definition in definitions:
+        sys.stdout.write("start processing '{0}'.\n".format(definition.outputPath))
+        runDefinition(definition)
+        sys.stdout.write("saved '{0}'.\n".format(definition.outputPath))
 
 
-if(__name__ == '__main__'):
+def runDefinition(definition: Definition):
+    result: Image.Image = Image.new("RGBA", (0, 0))
+
+    pixelProcessors: list[PixelProcessor] = []
+    for processor in definition.processors:
+        if isinstance(processor, PixelProcessor):
+            sys.stdout.write(
+                "\tEnqueue pixel processor '{0}'.\n".format(processor.name)
+            )
+            pixelProcessors.append(processor)
+        else:
+            if len(pixelProcessors) > 0:
+                sys.stdout.write("\tApply pixel processors.\n")
+                runPixelProcessors(pixelProcessors, result)
+                pixelProcessors.clear()
+        if isinstance(processor, ImageProcessor):
+            sys.stdout.write("\tApply image processor '{0}'.\n".format(processor.name))
+            result = processor.handleImage(result)
+    result.save(definition.outputPath)
+
+
+def runPixelProcessors(
+    processors: list[PixelProcessor], result: Image.Image
+) -> Image.Image:
+    draw: ImageDraw.ImageDraw = ImageDraw.Draw(result)
+    data: Final[list[tuple[int, int, int, int]]] = list(result.getdata())
+    xy: tuple[int, int] = (0, 0)
+    w = result.size[0]
+    i: int = 0
+    for pixel in data:
+        xy = (i % w, i // w)
+        for processor in processors:
+            processor.handlePixel(draw, xy, pixel)
+        i = i + 1
+    return result
+
+
+if __name__ == "__main__":
+    m1 = psutil.virtual_memory()
     run()
+    m2 = psutil.virtual_memory()
+    print(f"memory used {0}".format(m2.used - m1.used))
