b51b6cddb53eb1d54e07762200b04bc0496f8f15
128na <simutrans.128na@gmail.com>
2023-06-02 23:20:03 +0900

    環境変数設定

--- src/simutrans-image-merger.py
+++ src/simutrans-image-merger.py
@@ -2,6 +2,10 @@ import sys
 from typing import Final
 from PIL import Image, ImageDraw
 import argparse
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".env.ci")
 
 sys.path.append("./")
 
@@ -13,32 +17,32 @@ from src.MergeDefinition import (
 )
 
 from src.Logger import stdoutLogger, stderrLogger
-from src.version import version
 
 
 def run():
-    parser = argparse.ArgumentParser(
-        description="simutrans image merger version {0}.".format(version)
-    )
+    greeting = "simutrans image merger version {0}.".format(os.environ["APP_VERSION"])
+    parser = argparse.ArgumentParser(description=greeting)
     parser.add_argument("jsonPath", type=str, help="path to definition json file")
     args = parser.parse_args()
     try:
+        stdoutLogger.info(greeting)
+
         loader: Final[MergeDefinitionLoader] = MergeDefinitionLoader(args.jsonPath)
         definitions: Final[list[Definition]] = loader.getDefinitions()
 
         stdoutLogger.info(
-            "definition file load successed. version is {0}.".format(loader.version)
+            "Definition file load successed. version is {0}.".format(loader.version)
         )
 
         for definition in definitions:
             stdoutLogger.info(
-                "start processing for '{0}'.".format(definition.outputPath)
+                "Start processing for '{0}'.".format(definition.outputPath)
             )
             runDefinition(definition)
-            stdoutLogger.info("file saved '{0}'.".format(definition.outputPath))
+            stdoutLogger.info("File saved '{0}'.".format(definition.outputPath))
 
     except Exception as e:
-        stderrLogger.error("merge failed")
+        stderrLogger.error("Merge failed")
         raise e
 
 
@@ -48,18 +52,18 @@ def runDefinition(definition: Definition):
     pixelProcessors: list[PixelProcessor] = []
     for processor in definition.processors:
         if isinstance(processor, PixelProcessor):
-            stdoutLogger.info("Enqueue pixel processor '{0}'.".format(processor.name))
+            stdoutLogger.info("\tEnqueue pixel processor '{0}'.".format(processor.name))
             pixelProcessors.append(processor)
         else:
             if len(pixelProcessors) > 0:
-                stdoutLogger.info("Apply pixel processors.")
+                stdoutLogger.info("\tApply pixel processors.")
                 runPixelProcessors(pixelProcessors, result)
                 pixelProcessors.clear()
         if isinstance(processor, ImageProcessor):
-            stdoutLogger.info("Apply image processor '{0}'.".format(processor.name))
+            stdoutLogger.info("\tApply image processor '{0}'.".format(processor.name))
             result = processor.handleImage(result)
     if len(pixelProcessors) > 0:
-        stdoutLogger.info("Apply pixel processors.")
+        stdoutLogger.info("\tApply pixel processors.")
         runPixelProcessors(pixelProcessors, result)
 
     result.save(definition.outputPath)
